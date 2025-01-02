57d21ef3bc0db843d036e2cd764f09626ce48ea3
fjarr <142280610+fjarr@users.noreply.github.com>
2025-01-02 15:18:09 +0700

    Update index.vue

--- pages/profile/struktur/index.vue
+++ pages/profile/struktur/index.vue
@@ -1,8 +1,208 @@
 <template>
-    <div class="container-fluid">
-        STRUKTUR
+  <div class="container-fluid">
+    <h3 class="text-center mt-5 text-primary">STRUKTUR</h3>
+    <h3 class="text-center mb-3 text-secondary">SMKN 4 TASIKMALAYA</h3>
+    <div class="struktur p-5">
+      <div class="kepala d-flex justify-content-center">
+        <div class="card" style="width: 30%">
+          <p class="text-center">KEPALA SMKN 4 TASIKMALAYA</p>
+          <hr />
+          <p class="text-center">KURNIAWAN S.Pd M.Pd</p>
+        </div>
+      </div>
+      <div class="row">
+        <div class="col p-5">
+          <div class="card" style="width: 30%">
+            <p class="text-center">KETUA KOMITE</p>
+            <hr />
+            <p class="text-center">ABUN SULAIMAN S.Ag</p>
+          </div>
+        </div>
+      </div>
+      <div class="wakasek my-5">
+        <div class="row">
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">WAKASEK KESISWAAN</p>
+              <hr />
+              <p class="text-center">ACENG SARIPUDIN S.Si M.Pd</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">WAKASEK KURIKULUM</p>
+              <hr />
+              <p class="text-center">PAK AYI</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">WAKASEK HUMAS/HUBIN</p>
+              <hr />
+              <p class="text-center">ISEP WARTIMAN</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">WAKASEK SARANA/PRASARANA</p>
+              <hr />
+              <p class="text-center">SAEPUDIN S.Pd</p>
+            </div>
+          </div>
+        </div>
+        <div class="row my-5">
+          <div class="col-4">
+            <div class="card">
+              <p class="text-center">KEPALA PERPUSTAKAAN</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+          <div class="col-4">
+            <div class="card">
+              <p class="text-center">TENAGA PERPUSTAKAAN</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+        </div>
+        <div class="row my-5 d-flex justify-content-center">
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KAPROG TJKT</p>
+              <hr />
+              <p class="text-center">IBU</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KAPROG PPLG</p>
+              <hr />
+              <p class="text-center">TAUPIK HIDAYAT S.KOM</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KAPROG TSM</p>
+              <hr />
+              <p class="text-center">WAWAN SETIAWAN S.T</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KAPROG DKV</p>
+              <hr />
+              <p class="text-center">BU ANA</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KAPROG TOI</p>
+              <hr />
+              <p class="text-center">PAK ANA</p>
+            </div>
+          </div>
+        </div>
+        <div class="row my-5 d-flex justify-content-center">
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KABENG TJKT</p>
+              <hr />
+              <p class="text-center">PAK</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KABENG PPLG</p>
+              <hr />
+              <p class="text-center">PAK ZUL</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KABENG TSM</p>
+              <hr />
+              <p class="text-center">PAK</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KABENG DKV</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-2 p-3">
+            <div class="card">
+              <p class="text-center">KABENG TOI</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+        </div>
+        <div class="row d-flex justify-content-center">
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">BK</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">BK</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">BK</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">BK</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+          <div class="col-6 col-md-3 p-3">
+            <div class="card">
+              <p class="text-center">BK</p>
+              <hr />
+              <p class="text-center">BU</p>
+            </div>
+          </div>
+        </div>
+        <div class="row my-2 d-flex justify-content-center">
+          <div class="col-5">
+            <div class="card">
+              <p class="text-center">WALIKELAS</p>
+            </div>
+          </div>
+          <div class="col-5">
+            <div class="card">
+              <p class="text-center">GURU</p>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 <script setup>
-useHead({ title: "Struktur Organisasi" })
+useHead({Title:'Struktur'})
 </script>
+<style scoped>
+.kepala {
+  width: 100%;
+}
+@media (max-width: 700px) {
+  h5 {
+    font-size: 6px;
+  }
+}
+</style>
